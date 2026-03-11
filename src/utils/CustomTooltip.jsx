2802c970c4e6a158f9d2be6a9640b71e93fb1f5e
ashok-sw <ashok.arigala@apcfss.in>
2026-03-11 21:07:28 +0530

    changes in packages

--- src/utils/CustomTooltip.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import Tooltip from "@mui/material/Tooltip";
-import Zoom from "@mui/material/Zoom";
-const CustomTooltip = ({ direction, color, title, content }) => { return <Tooltip title={title} arrow TransitionComponent={Zoom} placement={direction} sx={{ "& .MuiTooltip-tooltip": { backgroundColor: "white !important", color: "black", fontSize: 14, borderRadius: 4, padding: 0, margin: 0 }, "& .MuiTooltip-arrow": { color: "white !important" } }}><span style={{ fontSize: "19px", cursor: "pointer", color: color }}>{content}</span></Tooltip>; };
-export default CustomTooltip;
